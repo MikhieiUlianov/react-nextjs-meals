8e74c88af706b39ba478df72760f79dd03983a14
Mkh <example@gmail.com>
2025-07-22 17:38:29 +0200

    using-server-actions

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -2,6 +2,16 @@ import ImagePicker from "@/components/Meals/ImagePicker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  async function shareMealsPage(formData: FormData) {
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("title"),
+      image: formData.get("image"),
+      creator: formData.get("creator"),
+      creator_email: formData.get("creator_email"),
+    };
+  }
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +21,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMealsPage}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +49,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
